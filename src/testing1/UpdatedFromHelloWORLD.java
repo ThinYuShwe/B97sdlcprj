5721c71c72af46f4031f9fcb3c47016c86946e92
HP <HP@DESKTOP-H5MLMTO>
2023-08-17 09:43:50 +0630

    Version 3

---
+++ src/testing1/UpdatedFromHelloWORLD.java
@@ -0,0 +1,10 @@
+package testing1;
+
+public class UpdatedFromHelloWORLD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Another class");
+	}
+
+}
